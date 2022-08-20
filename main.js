54613b82c9dbfe33edf16d135fdadac113b17548
willarmy20 <81721198+willarmy20@users.noreply.github.com>
2022-08-20 16:07:24 -0400

    finshed site

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
 window.addEventListener("scroll", () => {
-    // add/remove window.scrollY , depending on test conditional, window.scrollY less than 10
-    document.querySelector("nav").classList.toggle('window-scrolled', window.scrollY > 0); 
-})
+  // add/remove window.scrollY , depending on test conditional, window.scrollY less than 10
+  document
+    .querySelector("nav")
+    .classList.toggle("window-scrolled", window.scrollY > 0);
+});
 
 //CONTACT BUTTONS (circular text buttons)
-const textButtons = document.querySelectorAll('.contact_btn');
+const textButtons = document.querySelectorAll(".contact_btn");
 
-textButtons.forEach(textButton => {
-    let text = textButton.querySelector('p');
+textButtons.forEach((textButton) => {
+  let text = textButton.querySelector("p");
 
-    text.innerHTML = text.innerHTML.split('').map((character, index)=> `<span style="transform: rotate(${index * 12}deg)">${character}</span>`).join('')
-})
+  text.innerHTML = text.innerHTML
+    .split("")
+    .map(
+      (character, index) =>
+        `<span style="transform: rotate(${index * 12}deg)">${character}</span>`
+    )
+    .join("");
+});
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    599: {
+        slidesPerView: 2,
+        spaceBetween: 40
+    },
+    1023: {
+        slidesPerView: 3,
+        spaceBetween: 60
+    }
+  }
+});
+
+
+const nav = document.querySelector('.navLinks');
+const openNavBtn = document.querySelector('#nav_toggle-open');
+const close
